9e94d457e93d236a8f7146f0f481da6809b7bf2f
Jack Bicknell <jackbicknell@gmail.com>
2022-08-30 16:26:20 +0100

    Update exceptions.py

--- {{cookiecutter.project_slug}}/backend/core/exceptions.py
+++ {{cookiecutter.project_slug}}/backend/core/exceptions.py
@@ -14,7 +14,7 @@ class InvalidAuthToken(APIException):
     default_code = "invalid_token"
 
 
-class FirebaseError(APIException):
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+class FirebaseAuthenticationError(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
     default_detail = "The user provided with the auth token is not a valid Firebase user, it has no Firebase UID"
     default_code = "no_firebase_uid"
