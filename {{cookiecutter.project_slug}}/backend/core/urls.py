1ae372662e0c7df734583fbf799ba273d0145214
Jack Bicknell <jackbicknell@gmail.com>
2022-08-30 16:18:00 +0100

    Update urls.py

--- {{cookiecutter.project_slug}}/backend/core/urls.py
+++ {{cookiecutter.project_slug}}/backend/core/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from core import views
@@ -6,7 +5,7 @@ from core import views
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-
-urlpatterns = [path("", views.index)]
+urlpatterns = [
+    path("", views.index),
+    path("", include(router.urls)),
+]
